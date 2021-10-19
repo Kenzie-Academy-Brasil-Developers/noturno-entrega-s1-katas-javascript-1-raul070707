c0ed57dab6a2807658dfab8cb98656cda31c2fa8
Raul Ribeiro <raul260302@gmail.com>
2021-10-18 23:07:53 -0300

    Feito a entrega 42

--- src/katas1.js
+++ src/katas1.js
@@ -1,94 +1,125 @@
-function oneThroughTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result*/
-    
+/* function oneThroughTwenty(number) {
+  let resultado = []
+  for(let counter = 1; counter <= number; counter++){
+      resultado.push(counter)
+    }
+    return resultado
 }
+console.log(oneThroughTwenty(20)) */
 
-//call function oneThroughTwenty
 
-function evensToTwenty() {
-    
-   /* Your code goes below
-   Write a for or a while loop
-   return the result */
-  
+
+/* function evensToTwenty(number) {
+    let resultado2 = []
+    for(let counter = 1;counter <= number; counter++){
+      if (counter % 2 === 0){
+        resultado2.push(counter)
+      }
+    }
+    return resultado2
 }
+console.log(evensToTwenty(20)) */
 
-//call function evensToTwenty
 
-function oddsToTwenty() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
-   
+
+/* function oddsToTwenty(number) {
+   let resultado3 = []
+   for(let counter = 1;counter <= number; counter++){
+     if (counter % 2 !== 0){
+       resultado3.push(counter)
+     }
+   }
+   return resultado3
 }
+console.log(oddsToTwenty(20)) */
 
-//call function oddsToTwenty
 
-function multiplesOfFive() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
 
+/* function multiplesOfFive(number) {
+    let resultado4 = []
+    for(let counter = 1; counter <= number; counter ++){
+      if (counter % 5 === 0){
+        resultado4.push(counter)
+      }
+    }
+    return resultado4
 }
+console.log(multiplesOfFive(100)) */
 
-//call function multiplesOfFive
 
-function squareNumbers() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
-    
+
+/* function squareNumbers(number) {
+    let resultado5 = []
+    for(let counter = 1; counter <= number; counter++){
+      let conta = counter**2
+      if (conta <= number){
+        resultado5.push(conta)
+       }
+    }
+    return resultado5
+  }
+console.log(squareNumbers(100)) */
+
+
+
+/* function countingBackwards(number) {
+    let resultado6 = []
+    for(let counter = number; counter <= number && counter > 0; counter--){
+      resultado6.push(counter)
+    }
+    return resultado6
 }
+console.log(countingBackwards(20)) */
 
-//call function squareNumbers
 
-function countingBackwards() {
-    
-  /* Your code goes below
-  Write a for or a while loop
-  return the result */
+
+/* function evenNumbersBackwards(number) {
+    let resultado7 = []
+    for(counter = number; counter <= number && counter > 0; counter--){
+      if (counter % 2 === 0){
+        resultado7.push(counter)
+      }
+    }
+    return resultado7
 }
+console.log(evenNumbersBackwards(20)) */
 
-//call function countingBackwards
 
-function evenNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+
+/* function oddNumbersBackwards(number) {
+    let resultado8 = []
+    for(counter = number; counter <= number && counter > 0; counter--){
+      if (counter % 2 !== 0){
+        resultado8.push(counter)
+      }
+    }
+    return resultado8
 }
+console.log(oddNumbersBackwards(20)) */
 
-//call function evenNumbersBackwards
 
-function oddNumbersBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+
+/* function multiplesOfFiveBackwards(number) {
+    let resultado9 = []
+    for(counter = number; counter <= number && counter > 0; counter--){
+      if (counter % 5 === 0){
+        resultado9.push(counter)
+      }
+    }
+    return resultado9
 }
+console.log(multiplesOfFiveBackwards(100)) */
 
-//call function oddNumbersBackwards
 
-function multiplesOfFiveBackwards() {
-    
- /* Your code goes below
-  Write a for or a while loop
-  return the result */
+
+/* function squareNumbersBackwards(number) {
+    let resultado10 = []
+    for(counter = number; counter <= number && counter > 0; counter--){
+      let conta = counter**2
+      if (conta <= number){
+        resultado10.push(conta)
+      }
+    }
+    return resultado10
 }
-
-//call function multiplesOfFiveBackwards
-
-function squareNumbersBackwards() {
-    
-   /* Your code goes below
-  Write a for or a while loop
-  return the result */
-}
-
-//call function squareNumbersBackwards
+console.log(squareNumbersBackwards(100)) */
